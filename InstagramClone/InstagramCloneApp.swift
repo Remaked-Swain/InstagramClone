1b14d465f7671eb7a190eba0b2998a18493fd4a7
YunSwain <destap@naver.com>
2023-07-13 12:42:11 +0900

    Creation

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Swain Yun on 2023/07/13.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
